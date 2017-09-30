f0a6dd4e6f4c71e5319632f30db58eeb109de165
Kim HyunWoo <stories2@naver.com>
2017-09-30 18:54:32 +0900

    2-8 question : print max length name

--- Homework10-01/Homework10-01/2-8.cpp
+++ Homework10-01/Homework10-01/2-8.cpp
@@ -7,14 +7,23 @@ using namespace std;
 
 int main() {
 	char names[NAME_SIZE][BUFFER_SIZE] = { '\0', };
-	int i;
+	int i, maxLength = -0x7fffffff, maxLengthPoint;
 
 	cout << "5 명의 이름을 ';'으로 구분하여 입력하세요\n" << ">>";
 
 	for (i = 0; i < NAME_SIZE; i += 1) {
 		cin.getline(names[i], BUFFER_SIZE, ';');
-		cout << i << " : " << names[i] << endl;
+		cout << i + 1<< " : " << names[i] << endl;
+
+		int length = strlen(names[i]);
+
+		if (maxLength < length) {
+			maxLength = length;
+			maxLengthPoint = i;
+		}
 	}
+
+	cout << "가장 긴 이름은 " << names[maxLengthPoint] << endl;
 
 	return 0;
 }
