0bcc8788cb5a62caeacb26a640750ac7b83c3f1d
Kim HyunWoo <stories2@naver.com>
2017-10-22 16:13:55 +0900

    0405 question : solved

--- Homework10-25/Homework10-25/Circle.h
+++ Homework10-25/Homework10-25/Circle.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	void setRadius(int radius);
+	double getArea();
+};
+
+void Circle::setRadius(int radius) {
+	this->radius = radius;
+}
+
+double Circle::getArea() {
+	return radius * radius * 3.14;
+}
---
+++ Homework10-25/Homework10-25/main0405.cpp
@@ -0,0 +1,18 @@
+#include "Circle.h"
+
+#define CUT_LINE 100
+
+int main() {
+	Circle circle[3];
+	int i, overCutLine = 0;
+	for (i = 0; i < 3; i += 1) {
+		int radius;
+		cin >> radius;
+		circle[i].setRadius(radius);
+		if (circle[i].getArea() > CUT_LINE) {
+			overCutLine += 1;
+		}
+	}
+	cout << "면적이 " << CUT_LINE << "보다 큰 원은 " << overCutLine << "개 입니다." << endl;
+	return 0;
+}
